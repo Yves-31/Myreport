2c84260880ed7fb0f62d06510805a90ff4ab142d
aprennant 18 <aprennant18@MacBook-Pro-de-aprennant.local>
2022-06-15 16:08:10 +0200

    myfirst comit

---
+++ Complex/ComplexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexApp.swift
+//  Complex
+//
+//  Created by aprennant 18 on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct ComplexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
